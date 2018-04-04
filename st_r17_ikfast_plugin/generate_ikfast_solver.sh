5b072a9e6489da9b7c670ddbfad8b76c43934dd2
yycho0108 <jchocholate@gmail.com>
2018-04-03 23:26:18 -0400

    new IKFast, maybe better?

--- st_r17_ikfast_plugin/generate_ikfast_solver.sh
+++ st_r17_ikfast_plugin/generate_ikfast_solver.sh
@@ -10,5 +10,5 @@ source ~/.bashrc
 roscd st_r17_description/urdf
 rosrun collada_urdf urdf_to_collada robot_3.urdf ${OUTCOL}
 rosrun moveit_kinematics round_collada_numbers.py ${OUTCOL} ${OUTCOL} ${ROUND}
-python $(openrave-config --python-dir)/openravepy/_openravepy_/ikfast.py --robot=${OUTCOL} --iktype=translationdirection5d --baselink=0 --eelink=6 --savefile=${IKOUT}
+python $(openrave-config --python-dir)/openravepy/_openravepy_/ikfast.py --robot=${OUTCOL} --iktype=translationdirection5d --baselink=0 --eelink=5 --savefile=${IKOUT}
 popd
