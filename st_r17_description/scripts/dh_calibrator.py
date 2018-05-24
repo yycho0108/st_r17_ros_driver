f3ad232ff16c285939f39fb80f3e765e06314be5
Jamie Cho <jchocholate@gmail.com>
2018-05-24 06:36:49 -0400

    better targets and cleanup; TODO : accept multiple landmarks for better stability

--- st_r17_description/scripts/dh_calibrator.py
+++ st_r17_description/scripts/dh_calibrator.py
@@ -130,7 +130,7 @@ class DHCalibrator(object):
                 rpy0 = tf.assign(rpy_avg, rpy_avg_1)
                 self._T_init = tf.group(xyz0, rpy0)
 
-                gamma = 0.99
+                gamma = 0.9
 
                 xyz_avg_u = tf.assign(xyz_avg, gamma * xyz_avg + (1.0 - gamma) * xyz_avg_1)
                 rpy_avg_u = tf.assign(rpy_avg, gamma * rpy_avg + (1.0 - gamma) * rpy_avg_1)
@@ -177,7 +177,7 @@ class DHCalibrator(object):
         tf.summary.scalar('loss_xyz', self._loss_xyz)
         tf.summary.scalar('loss_rpy', self._loss_rpy)
 
-        self._writer = tf.summary.FileWriter('/tmp/dh/11', self._graph)
+        self._writer = tf.summary.FileWriter('/tmp/dh/13', self._graph)
         self._summary = tf.summary.merge_all()
 
     def eval_1(self, js, xs):
@@ -231,7 +231,7 @@ class DHCalibratorROS(object):
         self._dh0 = np.float32(self._dh0)
         z = np.random.normal(
                 loc = 0.0,
-                scale = [np.deg2rad(1.0), 0.01, 0.01, np.deg2rad(1.0)],
+                scale = [np.deg2rad(3.0), 0.03, 0.03, np.deg2rad(3.0)],
                 size = np.shape(self._dh0)
                 )
         dh0 = np.add(self._dh0, z)
@@ -308,129 +308,6 @@ def main():
     app = DHCalibratorROS()
     app.run()
 
-#
-#def build():
-#
-#
-#    # T 0 -> 1 ( n-1 == 0, n = 1 )
-#    # uses theta_1, d_1, alpha_0, a_0
-#
-#    # for T_0 -> T_1
-#    # alpha_0 along x_0 from z_0 to z_1
-#    # a_0 along x_0 from o_0 to o_1
-#    # d_1 along z_1
-#    # theta_1 along z_1
-#
-#    # full transformation
-#    #get_dh(index, alpha0, a0, d0)
-#
-#    dh01, q0 = get_dh(0, np.pi,    0,        -(0.033+0.322)         ) # base_link -> link_1
-#    dh12, q1 = get_dh(1, np.pi/2,  0,          0,         (-np.pi/2)) # 1 -> 2
-#    dh23, q2 = get_dh(2, 0,        0.375,      0                    ) # 2 -> 3
-#    dh34, q3 = get_dh(3, 0,        0.375, 0.024,        np.pi/2) # 3 -> 4
-#    dh45, q4 = get_dh(4, np.pi/2,  0.0,        0.042,            1.176) # 4 -> 5
-#    dh56, q5 = get_dh(5, 0,       -0.012,      0.159                ) # 5 -> camera
-#
-#    #dh0 = get_dh(0, np.pi/2, 0, 0.0) # link_1 -> link_2, i=0
-#    #dh1 = get_dh(1, 0, 0, 0.0) # link_2 -> link_3
-#    #dh2 = get_dh(2, 0.0, 0.375, 0) # link_3 -> link_4
-#    #dh3 = get_dh(3, np.pi/2, 0.375, 0) # link_4 -> link_5
-#    #dh4 = get_dh(4, np.pi/2, 0.042, 0.024) # link_5 -> camera_link
-#    #dh5 = get_dh(5, 0.0, 0.012, 0.159) # link_5 -> camera_link
-#
-#    T_f = tf.placeholder(dtype=tf.float32, shape=[None,4,4], name='T_f') # camera_link -> object
-#
-#    # account for offsets here, if any
-#    # notably, the offset at last joint
-#
-#    dhs = [dh01,dh12,dh23,dh34,dh45,dh56]
-#    qs = [q0, q1, q2, q3, q4, q5]
-#    Ts = [dh2T(*dh) for dh in dhs]
-#    Ts.append(T_f)
-#    T = reduce(lambda a,b : tf.matmul(a,b), Ts) # base_link -> object
-#    return T, qs, T_f
-#
-#def jcb(msg):
-#    global txn, rxn, qs, T, T_f, sess
-#
-#    js = np.concatenate((msg.position, [0]) )
-#
-#    feed_dict = {q:[j] for (q,j) in zip(qs, js)}
-#    feed_dict[T_f] = np.reshape(np.eye(4), (1,4,4))
-#
-#    _T = sess.run(T, feed_dict=feed_dict)
-#    txn = tx.translation_from_matrix(_T[0])
-#    rxn = tx.quaternion_from_matrix(_T[0])
-#
-#def main():
-#    global txn, rxn, qs, T, T_f, sess
-#    T, qs, T_f = build()
-#    loss = tf.square(T - tf.reduce_mean(T, axis=0, keep_dims=True))
-#    loss = tf.reduce_mean(loss)
-#    train = tf.train.GradientDescentOptimizer(learning_rate=1e-3).minimize(loss)
-#    sess = tf.Session()
-#
-#    feed_dict = {q:[0] for q in qs}
-#    feed_dict[T_f] = np.reshape(np.eye(4), (1,4,4))
-#    sess.run(tf.global_variables_initializer())
-#    _T = sess.run(T, feed_dict=feed_dict)
-#    txn = tx.translation_from_matrix(_T[0])
-#    rxn = tx.quaternion_from_matrix(_T[0])
-#
-#    rospy.init_node('dh_test')
-#    sub = rospy.Subscriber('/st_r17/joint_states', JointState, jcb)
-#    pub = rospy.Publisher('dh', PoseStamped, queue_size=10)
-#    msg = PoseStamped()
-#    msg.header.frame_id = 'base_link'
-#
-#
-#    rate = rospy.Rate(50)
-#    while not rospy.is_shutdown():
-#        msg.pose.position.x = txn[0]
-#        msg.pose.position.y = txn[1]
-#        msg.pose.position.z = txn[2]
-#
-#        msg.pose.orientation.x = rxn[0]
-#        msg.pose.orientation.y = rxn[1]
-#        msg.pose.orientation.z = rxn[2]
-#        msg.pose.orientation.w = rxn[3]
-#
-#        pub.publish(msg)
-#        rate.sleep()
-
-
 if __name__ == "__main__":
     main()
 
-#class DHCalibrator(object):
-#    def __init__(self):
-#        pass
-#
-#    @staticmethod
-#    def dh2T(alpha, a, d, q):
-#        """ Convert DH Parameters to Transformation Matrix """
-#        cq = cos(q)
-#        sq = sin(q)
-#        ca = cos(alpha)
-#        sa = sin(alpha)
-#
-#        T = Matrix([
-#            [cq, -sq, 0, a],
-#            [sq*ca, cq*ca, -sa, -sa*d],
-#            [sq*sa, cq*sa, ca, ca*d],
-#            [0, 0, 0, 1]
-#            ])
-#        return T
-#
-#    def _build(self, 
-#
-#    def dhs2T(dhs):
-#        Ts = [self.dh2T(*dh) for dh in dhs]
-#        T = reduce(lambda a,b: simplify(a*b), Ts)
-#        return T
-#
-#    def calibrator(dhs, qs, xs):
-#        T = dhs2T(dhs)
-#        psi = T.jacobian(q)
-#
-#def fk(q)
--- st_r17_description/scripts/target_publisher.py
+++ st_r17_description/scripts/target_publisher.py
@@ -7,11 +7,22 @@ from sensor_msgs.msg import JointState
 import tf.transformations as tx
 from fk import fk
 
+def fill_pose_msg(msg, txn, rxn):
+    msg.position.x = txn[0]
+    msg.position.y = txn[1]
+    msg.position.z = txn[2]
+
+    msg.orientation.x = rxn[0]
+    msg.orientation.y = rxn[1]
+    msg.orientation.z = rxn[2]
+    msg.orientation.w = rxn[3]
+
 class SimpleTargetPublisher(object):
     def __init__(self):
         rospy.init_node('simple_target_publisher')
 
-        self._dhs = [
+        # defaults
+        dhs = [
                 [np.pi, 0, -(0.033 + 0.322), 0],
                 [np.pi/2, 0, 0, -np.pi/2],
                 [0, 0.375, 0, 0],
@@ -19,16 +30,27 @@ class SimpleTargetPublisher(object):
                 [np.pi/2, 0, 0.042, 1.176],
                 [0, -0.012, 0.159, np.pi]
                 ]
-        self._dhs = np.float32(self._dhs)
+        xyz = np.random.uniform(low = -1.0, high = 1.0, size=3)
 
+        self._dhs = np.float32(dhs)
         self._rate = rospy.get_param('~rate', default=50)
         self._zero = rospy.get_param('~zero', default=False)
+
+        x = rospy.get_param('~x', default=xyz[0])
+        y = rospy.get_param('~y', default=xyz[1])
+        z = rospy.get_param('~z', default=xyz[2])
+        x,y,z = [np.round(e, decimals=3) for e in [x,y,z]]
+
+        self._xyz = [x,y,z]
+
+        rospy.loginfo('~xyz : {:.3f}, {:.3f}, {:.3f}'.format(x,y,z))
+
         self._ppub = rospy.Publisher('/target_pose', PoseStamped, queue_size=10)
         self._jpub = rospy.Publisher('/st_r17/joint_states', JointState, queue_size=10)
+        self._gpub = rospy.Publisher('/ground_truth', PoseStamped, queue_size=10)
 
     def publish(self):
         now = rospy.Time.now()
-        pmsg = PoseStamped()
 
         x = np.random.uniform(low=-np.pi, high=np.pi, size=5)
         if self._zero:
@@ -37,59 +59,31 @@ class SimpleTargetPublisher(object):
 
         ## base_link --> object
         M07 = tx.compose_matrix(
-                translate = (1, 0, 0)
+                translate = self._xyz
                 )# T_0_7
-        #M07 = np.eye(4)
+
+        ## base_link --> stereo_optical_link
         M06 = tx.compose_matrix(
                 angles = tx.euler_from_quaternion(rxn),
                 translate = txn) # T_0_6
 
-        # inverse ... 
-
-        # src = T_0_6
-        # trg = T_0_7
-
-        # 1 : M67
+        pmsg = PoseStamped()
         pmsg.header.frame_id = 'stereo_optical_link'
         M67 = np.matmul(np.linalg.inv(M06), M07)
         txn = tx.translation_from_matrix(M67)
         rxn = tx.quaternion_from_matrix(M67)
-
-        # 2 : M06
-        # pmsg.header.frame_id = 'base_link'
-        # txn = tx.translation_from_matrix(M06)
-        # rxn = tx.quaternion_from_matrix(M06)
-
-        #R = tx.quaternion_matrix(rxn)
-        #Rt = R.T
-        #M06i = np.copy(Rt)
-        #M06i[:3, 3] = -np.matmul(Rt[:3,:3], np.reshape(txn, (3,1)))[:, 0]
-
-        #M67 = np.matmul(M06i, M07)
-
-
-        #R = tx.rotation_matrix(tx.euler_from_quaternion(rxn))
-
-
-        #print np.linalg.inv(M)
-        #print Mi
-
-        #M = np.matmul(np.linalg.inv(M), M1)
-
-        #pmsg.header.frame_id = 'stereo_link'
-        #pmsg.header.frame_id = 'base_link'
         pmsg.header.stamp = now
 
-        pmsg.pose.position.x = txn[0]
-        pmsg.pose.position.y = txn[1]
-        pmsg.pose.position.z = txn[2]
-    
-        pmsg.pose.orientation.x = rxn[0]
-        pmsg.pose.orientation.y = rxn[1]
-        pmsg.pose.orientation.z = rxn[2]
-        pmsg.pose.orientation.w = rxn[3]
-
+        fill_pose_msg(pmsg.pose, txn, rxn)
         self._ppub.publish(pmsg)
+
+        gmsg = PoseStamped()
+        gmsg.header.frame_id = 'base_link'
+        txn = tx.translation_from_matrix(M07)
+        rxn = tx.quaternion_from_matrix(M07)
+        gmsg.header.stamp = now
+        fill_pose_msg(gmsg.pose, txn, rxn)
+        self._gpub.publish(gmsg)
 
         jmsg = JointState()
         jmsg.header.stamp = now
