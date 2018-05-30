ff2e50230f526207b527996146ff6c56973fc1ac
Jamie Cho <jchocholate@gmail.com>
2018-05-30 01:52:17 -0400

    more + better evaluation

--- st_r17_calibration/scripts/evaluate_dh.py
+++ st_r17_calibration/scripts/evaluate_dh.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python2
 
+import matplotlib
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import ListedColormap
+
 from st_r17_calibration.kinematics import fk
 
 import numpy as np
@@ -44,6 +47,35 @@ def characterize(txn_err, rxn_err):
 
     #plt.show()
 
+def characterize_v2(xyz, rpy, err):
+    xyz = np.float32(xyz)
+    rpy = np.float32(rpy)
+    fig = plt.figure()
+
+    cm0 = matplotlib.cm.get_cmap('cool')
+    cm = cm0(np.arange(cm0.N))
+    cm[:,-1] = np.linspace(0.1, 1, cm0.N)
+    cm = ListedColormap(cm)
+
+    ax_p = fig.add_subplot(121, projection='3d')
+    ax_p.set_xlabel('x')
+    ax_p.set_ylabel('y')
+    ax_p.set_zlabel('z')
+
+    n = len(xyz)
+    x,y,z = [xyz[:,i] for i in range(3)]
+    R,P,Y = [rpy[:,i] for i in range(3)]
+    s = ax_p.scatter(x,y,z, c=err, cmap=cm)
+    fig.colorbar(s, ax=ax_p)
+
+    ax_q = fig.add_subplot(122, projection='3d')
+    ax_q.set_xlabel('R')
+    ax_q.set_ylabel('P')
+    ax_q.set_zlabel('Y')
+
+    s = ax_q.scatter(R,P,Y, c=err, cmap=cm)
+    fig.colorbar(s, ax=ax_q)
+
 def plot_error(ax):
     e = np.loadtxt('/tmp/err.csv')
     ax.plot(e)
@@ -74,6 +106,8 @@ def main():
     print ('Final Error : ', _dh0 - _dh1)
     #_dh1 = np.concatenate((_dh1, _dh0[:, -1, np.newaxis]), axis=-1)
 
+    xyz = []
+    rpy = []
     d_xyz = []
     d_rpy = []
     for i in range(1000):
@@ -85,13 +119,19 @@ def main():
         rpy1 = tx.euler_from_quaternion(rxn1)
         d_xyz.append( np.subtract(txn0, txn1) )
         d_rpy.append( asub(rpy0, rpy1) )
+        xyz.append(txn0)
+        rpy.append(rxn0)
 
     d_xyz = np.float32(d_xyz)
+    d_rpy = np.float32(d_rpy)
+    err = np.linalg.norm(np.concatenate([d_xyz, d_rpy], axis=-1), axis=-1)
+
     d_rpy = np.rad2deg(np.float32(d_rpy))
 
     print('d_xyz', np.mean(np.abs(d_xyz), axis=0))
     print('d_rpy', np.mean(np.abs(d_rpy), axis=0))
     characterize(d_xyz, d_rpy)
+    characterize_v2(xyz, rpy, err)
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
